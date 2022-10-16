493bc23c420cdfe4e2feab179dfe4703c6339221
Sümeyye Kılıçoğlu <smyyeklcogl@icloud.com>
2022-10-16 19:38:52 +0300

    first commit

---
+++ simpleCalculater/ViewController.swift
@@ -0,0 +1,80 @@
+//
+//  ViewController.swift
+//  simpleCalculater
+//
+//  Created by Sümeyye Kılıçoğlu on 13.04.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var firstText: UITextField!
+    @IBOutlet weak var secondText: UITextField!
+    @IBOutlet weak var resultLabel: UILabel!
+    
+    var result = 0
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+
+    @IBAction func sumClicked(_ sender: Any) {
+        
+        if let firstNumber = Int(firstText.text!){
+            
+            if let secondNumber = Int(secondText.text!){
+            
+                result = firstNumber + secondNumber
+                resultLabel.text = String(result)
+            }
+    }
+      
+    }
+    @IBAction func minusClicked(_ sender: Any) {
+        
+        if let firstNumber = Int(firstText.text!){
+            
+            if let secondNumber = Int(secondText.text!){
+            
+                result = firstNumber - secondNumber
+                resultLabel.text = String(result)
+                
+            }
+    }
+      
+        
+    }
+    
+    @IBAction func multiplyClicked(_ sender: Any) {
+        
+        if let firstNumber = Int(firstText.text!){
+            
+            if let secondNumber = Int(secondText.text!){
+            
+                result = firstNumber * secondNumber
+                resultLabel.text = String(result)
+                
+            }
+    }
+      
+    }
+    @IBAction func divideClicked(_ sender: Any) {
+        
+        if let firstNumber = Int(firstText.text!){
+            
+            if let secondNumber = Int(secondText.text!){
+            
+                result = firstNumber / secondNumber
+                resultLabel.text = String(result)
+                
+            }
+    }
+      
+    }
+    
+}
+
